baaddc6cf9031ea35e2e92eeec08863f5962a920
Anders Magnusson <anders.magnusson@angrycreative.se>
2020-08-09 21:08:48 +0200

    remove unnecessary files and packages

--- index.js
+++
@@ -1,9 +0,0 @@
-const takeScreenshotMultiview = require( './take_screenshot_multiview' );
-const imgCompare = require( './compare' );
-const Scraper = require( './dist/Scraper' ).Scraper;
-
-module.exports = {
-	takeScreenshotMultiview,
-	imgCompare,
-	Scraper
-};
