5cc56f5e83952842add26e6621285587d4748ec0
fisknils <fishesgotfeelingsto@gmail.com>
2020-08-13 19:54:08 +0200

    .

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,35 @@
-declare module "puppeteer-regression-testing";
+declare module "puppeteer-regression-testing" {
+  export type InjectJS = {
+    enabled: boolean;
+    script: string;
+  };
+
+  export type ScreenshotDiff = {
+    Width: number;
+    Path: string;
+    Base64: string | null;
+    Screenshots: Array<Screenshot>;
+    misMatchPercentage: number;
+    isSameDimensions: boolean;
+    DOMCountDiff: number;
+  };
+
+  export type Screenshot = {
+    Base64: string;
+    URL: string;
+    Width: number;
+    DOMCount: number;
+  };
+
+  export type IncompleteScreenshot = {
+    Base64: string | null;
+    URL: string | null;
+    Width: number | null;
+  };
+
+  export type Job = {
+    URLs: URL[];
+    Viewports: Array<number>;
+    InjectJS: InjectJS;
+  };
+}
